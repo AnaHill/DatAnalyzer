c91370b230c4449b10b59a7de8926f87c86e6115
Antti Mäki <aj_maki@hotmail.com>
2022-05-09 12:43:07 +0300

    Create Update_DataInfo
    Function to update DataInfo.
    This Update_DataInfo function replaces previous Update_DataInfo_datacol_and_time_names.m

---
+++ part1_raw_data_handling/update_DataInfo.m
@@ -0,0 +1,71 @@
+function DataInfo = update_DataInfo(DataInfo)
+%% Update DataInfo: update_DataInfo.m
+% .file_names = file names 
+% .datacol_numbers  = datacolumn number, for MEA from raw data datacolumn numbers taken
+% .datacol_names = naming thse datacolumns
+% .measurement.time.names = adding "measurement name" for legend purposes
+narginchk(0,1)
+nargoutchk(0,1)
+
+if nargin < 1 || isempty(DataInfo)
+    try
+        DataInfo = evalin('base', 'DataInfo');
+    catch
+        error('No proper DataInfo')
+    end
+end
+
+
+% file names if only .file_names_mat exist
+try
+    DataInfo.file_names;
+catch
+    file_numbers_to_analyze = 1:DataInfo.files_amount;
+    DataInfo.file_names = DataInfo.file_names_mat(file_numbers_to_analyze);
+end
+
+% datacol_numbers
+try
+    DataInfo.datacol_numbers;
+catch
+    disp('Creating DataInfo.datacol_numbers')
+    try
+        DataInfo.datacol_numbers = DataInfo.MEA_columns;
+    catch
+        try
+            Data = evalin('base', 'Data');
+        catch
+            error('No proper Data')
+        end
+        DataInfo.datacol_numbers = 1:length(Data{1,1}.data(1,:));
+    end
+    DataInfo.datacol_numbers
+end
+
+% datacol_names
+try
+    DataInfo.datacol_names;
+catch
+    disp('Creating DataInfo.datacol_names')
+    for pp=1:length(DataInfo.datacol_numbers)
+        try
+            DataInfo.datacol_names{pp,1} = ['MEAele',...
+                num2str(DataInfo.MEA_electrode_numbers(pp))];                
+        catch
+            DataInfo.datacol_names{pp,1} = ['DataCol',...
+                num2str(DataInfo.datacol_numbers(pp))];                
+        end
+    end
+    DataInfo.datacol_names
+end
+
+% add "measurement name" to DataInfo.measurement.time for the legends
+try
+    DataInfo.measurement_time.names;
+catch
+    disp('Creating "measurement name" to DataInfo.measurement.time for the legends')
+    DataInfo = create_time_names_for_DataInfo(DataInfo);
+end
+
+
+
