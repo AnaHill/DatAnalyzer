b7c37840c5673368e85f59b4b0a7b72b47fe5943
Antti Mäki <aj_maki@hotmail.com>
2022-05-12 15:28:48 +0300

    Fixed plot_quick_BPM_summary_plot function

--- plotting/plot_quick_BPM_summary_plot.m
+++ plotting/plot_quick_BPM_summary_plot.m
@@ -6,6 +6,10 @@ function plot_quick_BPM_summary_plot(bpm_or_amplitude,DataInfo, Data_BPM_summary
 narginchk(0,3)
 nargoutchk(0,0)
 
+if nargin < 1 || isempty(bpm_or_amplitude)
+    bpm_or_amplitude = 'bpm';
+end
+
 if nargin < 2 || isempty(DataInfo)
     try
         DataInfo = evalin('base', 'DataInfo');
@@ -22,10 +26,6 @@ if nargin < 3 || isempty(Data_BPM_summary)
     end
 end
 
-if nargin < 1 || isempty(bpm_or_amplitude)
-    bpm_or_amplitude = 'bpm';
-end
-
 switch bpm_or_amplitude
     case 'bpm'
         data_to_plot = Data_BPM_summary.BPM_avg;
@@ -37,14 +37,14 @@ end
 
 datetime=DataInfo.measurement_time.datetime;
 plot(datetime, data_to_plot,'marker','*')
+title([DataInfo.experiment_name,' - ',DataInfo.measurement_name],'interpreter','none')
+ylabel(ylabel_text)
 try
     hs=DataInfo.hypoxia.start_time_index;
     he=DataInfo.hypoxia.end_time_index;
     hold all,
     plot([datetime(hs) datetime(hs)],[min(data_to_plot(:)) max(data_to_plot(:))] ,'--','color',[.4 .4 .4])
     plot([datetime(he) datetime(he)],[min(data_to_plot(:)) max(data_to_plot(:))] ,'--','color',[.4 .4 .4])
-    title([DataInfo.experiment_name,' - ',DataInfo.measurement_name],'interpreter','none')
-    ylabel(ylabel_text)
 catch
     disp('No hypoxia info in DataInfo.hypoxia')
 end
