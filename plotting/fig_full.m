14cdfe1b6c651c82f634b49a91227e86825c7891
Antti Mäki <aj_maki@hotmail.com>
2022-05-18 07:57:08 +0300

    Update fig _full and _half functions

--- plotting/fig_full.m
+++ plotting/fig_full.m
@@ -1,25 +1,7 @@
-% update 2022/02
-% previously did not work correctly, if one monitor is closed after MATLABs start
+% Will create full screen size figure
 % see: https://se.mathworks.com/matlabcentral/answers/102219-how-do-i-make-a-figure-full-screen-programmatically-in-matlab
-hfig = figure();hfig.WindowState = 'maximized';zoom on;
+hfig = figure();
+hfig.WindowState = 'maximized';
+zoom on;
 
-% jotain valittaa javaframesta, alla oleva ei toiminut
-% hfig = figure; pause(0.00001); frame_h = get(handle(hfig),'JavaFrame');
-% set(frame_h,'Maximized',1);
-
-% ehkä myös maximize funktion käyttö
-% https://se.mathworks.com/matlabcentral/fileexchange/25471-maximize
-
-%% older
-    % MP = get(0, 'MonitorPositions');
-    % found_monitor_numbers = 1:length(MP(:,1));
-    % fig_ind = find(max(MP(:,3)) == MP(:,3),1);
-    % fig_out_pos = [MP(fig_ind,:)];
-    % hfig = figure('units','pixel','outerposition',[fig_out_pos]);
-    % zoom on;
-    % clear found_monitor_numbers MP fig_ind fig_out_pos
-
-
-% set(hfig, 'Visible','off'), disp('set figure hfig visible off')
-% set(hfig, 'Visible','on'),
 
--- plotting/fig_half.m
+++ plotting/fig_half.m
@@ -1,6 +1,7 @@
-%% update 2022/05: previous sometimes "too fast" so 
-% result was not half screen size
-hfig = figure;org_size = hfig.OuterPosition;
+% Will create (approximately) half screen size figure
+% First, creates full size figure, which is then reduced to half
+% some delay is added that resize really works
+hfig = figure();org_size = hfig.OuterPosition;
 hfig.WindowState = 'maximized';
 % because of some delay with WindowState = 'maximed'
 % first wait enough that figure is full size
@@ -22,34 +23,4 @@ set(hfig,'OuterPosition',[full_size_fig_parameters(1) + ...
     full_size_fig_parameters(2)+round(full_size_fig_parameters(4)/3), ...
     round(full_size_fig_parameters(3)/2),round(full_size_fig_parameters(4)/2)]);
 pause(0.1)
-%% update 2022/02
-% previously did not work correctly, if one monitor is closed after MATLABs start
-% see: https://se.mathworks.com/matlabcentral/answers/102219-how-do-i-make-a-figure-full-screen-programmatically-in-matlab
-% hfig = figure();hfig.WindowState = 'maximized';zoom on;
-% pause(.2) % pause is needed for resize to work properly
-% set(hfig, 'OuterPosition',[hfig.OuterPosition(1)+round(hfig.OuterPosition(3)/3), ...
-%     hfig.OuterPosition(2)+round(hfig.OuterPosition(4)/3), ...
-%     round(hfig.OuterPosition(3)/2),round(hfig.OuterPosition(4)/2)])
-%% tryout
-% hfig = figure();hfig.WindowState = 'maximized';zoom on;
-% fig_out_pos = hfig.OuterPosition;
-% fig_out_pos(1) = fig_out_pos(1) +  round(fig_out_pos(3)/3);
-% fig_out_pos(2) = fig_out_pos(2) +  round(fig_out_pos(4)/3);
-% fig_out_pos(3:4) = round(fig_out_pos(3:4)/2);
-% hfig.OuterPosition = fig_out_pos;
-% set(hfig, 'OuterPosition',[hfig.OuterPosition(1:2) hfig.OuterPosition(3:4)/2])
-% set(hfig, 'OuterPosition',fig_out_pos)
-% clear fig_out_pos
-%% older
-% MP = get(0, 'MonitorPositions');
-% found_monitor_numbers = 1:length(MP(:,1));
-% fig_ind = find(max(MP(:,3)) == MP(:,3),1);
-% fig_out_pos = [MP(fig_ind,:)];
-% % half in the middle
-% fig_out_pos(1) = fig_out_pos(1) +  round(fig_out_pos(3)/3);
-% fig_out_pos(2) = fig_out_pos(2) +  round(fig_out_pos(4)/3);
-% fig_out_pos(3:4) = round(fig_out_pos(3:4)/2);
-% 
-% hfig = figure('units','pixel','outerposition',[fig_out_pos]);
-% zoom on;
-% clear found_monitor_numbers MP fig_ind fig_out_pos
+zoom on
