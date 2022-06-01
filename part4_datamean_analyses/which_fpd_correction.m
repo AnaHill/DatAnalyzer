3a8be0a127ea2874dc7bac7949dc8a1c1a9c2c1d
Antti Mäki <aj_maki@hotmail.com>
2022-06-01 09:25:28 +0300

    Update which_fpd_correction
    which_fpd_correction has now data_in as first input

--- part4_datamean_analyses/which_fpd_correction.m
+++ part4_datamean_analyses/which_fpd_correction.m
@@ -1,19 +1,18 @@
-function [dat, tittext] = which_fpd_correction(fpd_correction_equation,...
-    dat, Data_BPM_summary)
-% function [dat, tittext] = which_fpd_correction(fpd_correction_equation)
+function [data_out, tittext] = which_fpd_correction(data_in, fpd_correction_equation,...
+    Data_BPM_summary)
+% function [data_out, tittext] = which_fpd_correction(data_in, fpd_correction_equation,Data_BPM_summary)
 % calculates fpd correction based on user input
 % Options: 
-    % 1) (default) Izumi-Nakaseko: FPDc=FPD/(60/BPM)^{0.22}
-        % https://doi.org/10.1016/j.jphs.2017.08.008
+    % 1) Izumi-Nakaseko (default): FPDc=FPD/(60/BPM)^{0.22}
+        % more info: https://doi.org/10.1016/j.jphs.2017.08.008
     % 2) Bazett: FPDc=FPD/(60/BPM)^{1/2}
-        % 
     % 3) Fridericia: FPDc=FPD/(60/BPM)^{1/3}
-    % 4) Pure FPD
+    % 4) Other: Pure FPD
 
-narginchk(0,3)
+narginchk(1,3)
 nargoutchk(0,2)
 
-if nargin < 1 || isempty(fpd_correction_equation)
+if nargin < 2 || isempty(fpd_correction_equation)
     fpd_correction_equation = 'Izumi-Nakaseko';
 end
 
@@ -23,19 +22,25 @@ end
 
 switch fpd_correction_equation
     case 'Izumi-Nakaseko'
-        dat = dat./(Data_BPM_summary.BPM_avg/60).^0.22; % \cite{Hyyppa2018}: Izumi-Nakeseko 2017
-        tittext = ['BMP corrected signal duration',10,'FPDc=FPD/(60/BPM)^{0.22} (ms)'];
+        % \cite{Hyyppa2018}: Izumi-Nakeseko 2017
+        data_out = data_in ./ (Data_BPM_summary.BPM_avg/60).^0.22; 
+%         tittext = ['BMP corrected signal duration',10,'FPDc=FPD/(60/BPM)^{0.22} (ms)'];
+%         tittext = [10,'Izumi-Nakaseko FPDc=FPD/(60/BPM)^{0.22} (ms)'];
+        tittext = [10,'FPDc=FPD/(60/BPM)^{0.22}'];
         disp('Choosing Izumi-Nakaseko: FPDc=FPD/(60/BPM)^{0.22}')
     case 'Bazett'
-        dat = dat./sqrt(Data_BPM_summary.BPM_avg/60);
-        tittext = ['BMP corrected signal duration',10,'Bazetts FPDc=FPD/(60/BPM)^{1/2} (ms)'];
+        data_out = data_in ./ sqrt(Data_BPM_summary.BPM_avg/60);
+%         tittext = [10,'Bazetts FPDc=FPD/(60/BPM)^{1/2} (ms)'];
+        tittext = [10,'FPDc=FPD/(60/BPM)^{1/2}'];
         disp('Choosing Bazetts: FPDc=FPD/(60/BPM)^{1/2}')
     case 'Fridericia'
-        dat = dat./(Data_BPM_summary.BPM_avg/60).^(1/3);
-        tittext = ['BMP corrected signal duration',10,'Fridericia FPDc=FPD/(60/BPM)^{1/3} (ms)'];
+        data_out = data_in ./ (Data_BPM_summary.BPM_avg/60).^(1/3);
+%         tittext = [10,'Fridericia FPDc=FPD/(60/BPM)^{1/3} (ms)'];
+        tittext = [10,'FPDc=FPD/(60/BPM)^{1/3}'];
         disp('Choosing Fridericia: FPDc=FPD/(60/BPM)^{1/3}')
     otherwise
-        disp('No proper FPDc chosen:') % dat = dat;
+        disp('No proper FPDc formula chosen: using FPD')
+        data_out = data_in; 
         tittext = ['FPD'];    
 end
 
