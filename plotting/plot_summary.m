fff13cb0a11c2ded6b3d600f5c476ee0f9edd5db
Antti Mäki <aj_maki@hotmail.com>
2022-06-01 09:22:37 +0300

    Create plot_summary
    Rewrote summary plot function;
    Function plot_DataPeaks_summary
    replace by function plot_summary

--- plotting/plot_summary.m
+++ plotting/plot_summary.m
@@ -1,8 +1,52 @@
-function hfig = plot_summary(normalized, fpd_correction, time_unit)
-narginchk(0,2)
+function hfig = plot_summary(normalizing, fpd_correction, time_unit, hfig,...
+    DataInfo, Data_BPM_summary, DataPeaks_summary, Data_o2)
+% PLOT_SUMMARY plots summary of the BPM analysis
+% plot_summary % default plot with absolute values and 
+    % - fpd-correction: Izumi-Nakaseko FPDc
+    % - time_unit = 'datetime';
+    % - to new, full screen size, figure
+% plot_summary(1) % normalized values
+% plot_summary([],[],[],hfig) % if plotting to certain figure given in input
+% fpd_correction: Following FPDc equations
+    % 1) 'Izumi-Nakaseko' (default): FPDc=FPD/(60/BPM)^{0.22}
+    % 2) 'Bazett': FPDc=FPD/(60/BPM)^{1/2}
+    % 3) 'Fridericia': FPDc=FPD/(60/BPM)^{1/3}
+    % 4) 'none' (or any other than above): Pure FPD, no corrected FPD
+% time_unit   
+    % 'datetime' (default)
+    % 'hours'
+    % 'seconds'
+    % 'file_index'
+% required files
+    % choose_timep_unit.m
+        % [timep, xlabel_text] = choose_timep_unit(timep_unit,DataInfo)
+    % which_fpd_correction.m
+        % [data_out, tittext] = which_fpd_correction(data_in, fpd_correction, Data_BPM_summary)
+    % plot_hypoxia_line
+        % plot_hypoxia_line(timep, dat, DataInfo)    
+%% TODO:
+% see script open('.\plotting\Summarize_plot_DataPeaks_summary.m')
+    % 1) user could choose index(es) to be normalized
+    % 2) If want to limit y-axis to certain range, could give them in input
+    % ylimits = [0 2.5];
+    % ylimits_o2 = [0 1.1];
+    % for pp = 1:how_many_different_data
+    % 	subplot(how_many_different_data,1,pp)
+    %     try
+    %         Data_o2.data(Data_o2.measurement_time.datafile_index)
+    %         yyaxis left
+    %         ylim([ylimits])
+    %         yyaxis right
+    %         ylim([ylimits_o2])
+    %     catch
+    %         ylim([ylimits])
+    %     end
+    % end
+%% checking inputs and 
+narginchk(0,8)
 nargoutchk(0,1)
-
-if nargin < 1 || isempty(normalized)
+% set defaults
+if nargin < 1 || isempty(normalizing)
     normalizing = 0;
 end
 if any(normalizing ~= 1)
@@ -12,37 +56,56 @@ end
 if nargin < 2 || isempty(fpd_correction)
     fpd_correction = 'Izumi-Nakaseko';
 end
+
 if nargin < 3 || isempty(time_unit)
     time_unit = 'datetime';  
 end
 
-% get variables
-try
-    DataInfo = evalin('base', 'DataInfo');
-catch
-    error('No DataInfo found!')
-end
-try
-    Data_BPM_summary = evalin('base', 'Data_BPM_summary');
-catch
-    error('No Data_BPM_summary found!')
-end
-try
-    DataPeaks_summary = evalin('base', 'DataPeaks_summary');
-catch
-    error('No DataPeaks_summary found!')
+if nargin < 4 || isempty(hfig)
+    % hfig = evalin('base', 'hfig'); % does this needed?!
+    disp('Create new full size figure.')
+    fig_full % creates hfig
 end
 
-try
-    Data_o2 = evalin('base', 'Data_o2');
-catch
-    disp('No O2 info, Data_o2, found')
+%% read data and info if not given
+if nargin < 5 || isempty(DataInfo)
+    try
+        DataInfo = evalin('base', 'DataInfo');
+    catch
+        error('No proper DataInfo')
+    end
 end
 
+if nargin < 6 || isempty(Data_BPM_summary)
+    try
+        Data_BPM_summary = evalin('base', 'Data_BPM_summary');
+    catch
+        error('No proper Data_BPM_summary')
+    end
+end
+
+if nargin < 7 || isempty(DataPeaks_summary)
+    try
+        DataPeaks_summary = evalin('base', 'DataPeaks_summary');
+    catch
+        error('No proper DataPeaks_summary')
+    end
+end
+
+if nargin < 8 || isempty(Data_o2)
+    try
+        Data_o2 = evalin('base', 'Data_o2');
+    catch
+        disp('No O2 info (Data_o2), found')
+    end
+end
+
+%%
+% get time units
 [timep, xlabel_text] = choose_timep_unit(time_unit,DataInfo);
 
-
-%%% TODO: user could choose index(es) to be normalized
+% get normalizing index(es) if used
+% TODO: user could choose index(es) to be normalized
 if normalizing == 1
     % choosing index(es) which values are used for normalizing values
     try % finding hypoxia starting time index
@@ -59,9 +122,10 @@ if normalizing == 1
     end    
 end
 
-%%%%% plotting
-fig_full    
-sgtitle([DataInfo.experiment_name,10, DataInfo.measurement_name],'interpreter','none')
+%% Plotting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure(hfig)% fig_full    
+sgtitle([DataInfo.experiment_name,10, DataInfo.measurement_name],...
+    'interpreter','none')
 %% if absolute values plotted
 if normalizing == 0
     try
@@ -90,7 +154,7 @@ if normalizing == 0
 
     % calculate FPDc (or FPD)
     dat =  DataPeaks_summary.fpd; 
-    [dat, tittext] = which_fpd_correction(fpd_correction, dat);
+    [dat, tittext] = which_fpd_correction(dat,fpd_correction);
 
     plot(timep, dat,'.-'), ylabel(tittext), title(tittext)
     plot_hypoxia_line(timep, dat, DataInfo)
@@ -110,7 +174,7 @@ if normalizing == 0
         axis tight
     end
 end
-%% if normalized values
+%% if normalized values plotted
 if normalizing == 1
     try
         how_many_different_data = 3;
@@ -123,7 +187,7 @@ if normalizing == 1
         
         subplot(how_many_different_data,1,2)
         dat = abs(DataPeaks_summary.depolarization_amplitude);
-        tittext = 'Normalized absolute amplitude (norm)';
+        tittext = 'Absolute amplitude (norm)';
         dat = dat ./ mean(dat(norm_index,:));
         plot(timep, dat), ylabel(tittext), title(tittext)
         plot_hypoxia_line(timep, dat, DataInfo)
@@ -143,8 +207,10 @@ if normalizing == 1
     subplot(how_many_different_data,1,how_many_different_data)
     % calculate FPDc (or FPD)
     dat =  DataPeaks_summary.fpd;
-    [dat, titletext_fpdctype] = which_fpd_correction(fpd_correction, dat);
-    tittext = ['Normalized signal duration fpd(c) based on ',titletext_fpdctype];
+    [dat, titletext_fpdctype] = which_fpd_correction(dat, fpd_correction);
+%     tittext = ['Normalized signal duration fpd(c) based on ',titletext_fpdctype];
+    tittext = ['FPDc (norm)',titletext_fpdctype];
+
     % normalizing
     dat = dat./ mean(dat(norm_index,:));
     plot(timep, dat,'.-'), ylabel(tittext), title(tittext)
@@ -166,8 +232,5 @@ if normalizing == 1
         axis tight
     end
 end
-
-
-
 
 end
