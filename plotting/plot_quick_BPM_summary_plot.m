ed3aa9fa016a1114a9b5467f2b967039be8a8cac
Antti Mäki <aj_maki@hotmail.com>
2022-06-28 06:10:13 +0300

    Include time unit in plot_quick_BPM_summary_plot

--- plotting/plot_quick_BPM_summary_plot.m
+++ plotting/plot_quick_BPM_summary_plot.m
@@ -1,9 +1,9 @@
-function plot_quick_BPM_summary_plot(bpm_or_amplitude,DataInfo, Data_BPM_summary) 
-% function plot_quick_BPM_summary_plot(bpm_or_amplitude,DataInfo, Data_BPM_summary) 
+function plot_quick_BPM_summary_plot(bpm_or_amplitude,DataInfo, Data_BPM_summary, time_unit) 
+% function plot_quick_BPM_summary_plot(bpm_or_amplitude,DataInfo, Data_BPM_summary, time_unit) 
 % fig_full, plot_quick_BPM_summary_plot() % plots bmp
 % fig_full, plot_quick_BPM_summary_plot('amp')
 % fig_half, plot_quick_BPM_summary_plot('amp')
-narginchk(0,3)
+narginchk(0,4)
 nargoutchk(0,0)
 
 if nargin < 1 || isempty(bpm_or_amplitude)
@@ -26,6 +26,10 @@ if nargin < 3 || isempty(Data_BPM_summary)
     end
 end
 
+if nargin < 4 || isempty(time_unit)
+    time_unit = 'datetime';
+end
+
 %%%
 switch bpm_or_amplitude
     case 'bpm'
@@ -46,9 +50,13 @@ for col_index = 1:length(DataInfo.datacol_numbers)
     end
 end
 dataplots = [];
-
-datetime=DataInfo.measurement_time.datetime;
-dataplots = plot(datetime, data_to_plot,'marker','*');
+if strcmp(time_unit,'datetime')
+    time_vector=DataInfo.measurement_time.datetime;
+elseif strcmp(time_unit,'duration')
+    time_vector=DataInfo.measurement_time.duration;
+    % TODO: muut
+end
+dataplots = plot(time_vector, data_to_plot,'marker','*');
 title([DataInfo.experiment_name,' - ',DataInfo.measurement_name],'interpreter','none')
 ylabel(ylabel_text)
 try
@@ -61,9 +69,9 @@ try
     hs=DataInfo.hypoxia.start_time_index;
     he=DataInfo.hypoxia.end_time_index;
     hold all,
-    plot([datetime(hs) datetime(hs)],...
+    plot([time_vector(hs) time_vector(hs)],...
         [min(data_to_plot(:)) max(data_to_plot(:))] ,'--','color',[.4 .4 .4])
-    plot([datetime(he) datetime(he)],...
+    plot([time_vector(he) time_vector(he)],...
         [min(data_to_plot(:)) max(data_to_plot(:))] ,'--','color',[.4 .4 .4])
 catch
     disp('No hypoxia info in DataInfo.hypoxia')
