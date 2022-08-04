aa8f3407065aff915f1a57a04449e793bc373d2a
Antti Mäki <aj_maki@hotmail.com>
2022-08-04 09:10:41 +0300

    Add option to print or not if no hypoxia line

--- plotting/plot_hypoxia_line.m
+++ plotting/plot_hypoxia_line.m
@@ -1,4 +1,10 @@
-function plot_hypoxia_line(timep, dat, DataInfo)
+function plot_hypoxia_line(timep, dat, DataInfo, disp_if_no_hypoxia_line)
+narginchk(3,4)
+% default: do not print  "no hypoxia_line"
+if nargin < 4 || isempty(disp_if_no_hypoxia_line)
+    disp_if_no_hypoxia_line = 0;
+end
+disp_if_no_hypoxia_line = disp_if_no_hypoxia_line(1);
 try
     hyp_start = DataInfo.hypoxia.start_time_index;
     hyp_end = DataInfo.hypoxia.end_time_index;
@@ -7,6 +13,8 @@ try
     line([timep(hyp_end) timep(hyp_end)], [min(dat(:)) max(dat(:))],...
         'color', [.4 .4 .4],'linestyle','--')
 catch
-    disp('no hypoxia information to plot')
+    if disp_if_no_hypoxia_line(1) ~= 0
+        disp('no hypoxia information to plot')
+    end
 end
 end
