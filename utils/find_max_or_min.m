50f670466e68c63dfc752e337b3dfce7f06ecb6c
Antti Mäki <aj_maki@hotmail.com>
2022-08-26 12:01:26 +0300

    Update example

--- utils/find_max_or_min.m
+++ utils/find_max_or_min.m
@@ -2,8 +2,12 @@ function [values,location_indexes] = find_max_or_min(data, start_index, ...
     location_window_start_and_end_indexes, max_or_min_value)
 % function [values,location_indexes] = find_max_or_min(data, start_index, ...
 %     location_window_start_and_end_indexes, max_or_min_value)
-% Finds max or min value without filtering data
+% Finds max or min value of data
 % Example: data = DataPeaks_mean{1, 1}.data(:,1); [value, location] = find_max_or_min(data)
+% if want to use for filtered data, use e.g.
+% data_filtered = filter_data(data, fs, used_filter, filter_parameters,[],'no');
+% [value_filtered_data, location_filtered_data] = find_max_or_min(data_filtered)
+
 narginchk(1,4)
 nargoutchk(0,2)
 % assumes that each column in data is separate
@@ -28,7 +32,8 @@ if start_index < 1 || start_index > length(data)
 end
 
 % location_window_start_and_end_indexes: default: set to start_index to end
-if nargin < 3 || isempty(location_window_start_and_end_indexes)
+if nargin < 3 || isempty(location_window_start_and_end_indexes) ...
+        || isnan(location_window_start_and_end_indexes)
     location_window_start_and_end_indexes = [start_index length(data)];
 end
 if length(location_window_start_and_end_indexes) > 2
@@ -58,16 +63,18 @@ if nargin < 4 || isempty(max_or_min_value)
     disp('Finding maximum value')
 end
 if ~strcmp(max_or_min_value,'max') && ~strcmp(max_or_min_value,'min')
-   disp('Incorrect max_or_min given. Set to max, i.e. finding positive peak')
+   disp('Incorrect max_or_min given. Set to max, i.e. finding the highest positive value')
    max_or_min_value = 'max';
 end
 
 window_indexes = [min(location_window_start_and_end_indexes):...
     max(location_window_start_and_end_indexes)];
+
+% finding max or min value between given window
 if strcmp(max_or_min_value,'max')
     [values,location_indexes] = max(data(window_indexes,:)); 
 else
     [values,location_indexes] = min(data(window_indexes,:)); 
 end
-location_indexes = location_indexes + window_indexes(1) - 1;    
+location_indexes = location_indexes - 1 + window_indexes(1);    
 end
